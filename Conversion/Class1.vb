387cd60c33331af7f22cd4ab1d90d98510e0135e
Liyanage Dilanga <qqslbd@gmail.com>
2018-10-05 03:43:59 +0530

    initial release
    initial release

---
+++ Conversion/Class1.vb
@@ -0,0 +1,71 @@
+﻿'Developed by Badde Liyanage Don Dilanga. bld@dilanga.com. 
+
+Public Class UnitConversion
+
+    '//In here there are 
+    '//Inch -> cm
+    '//m/s -> km/h
+    '//mmHg -> Pa
+    ''//Hp -> W
+    '//J ->Cal
+
+    'Inch -> cm (IntoCm)
+    'cm -> Inch (CmtoIn)
+
+    'ms -> kmh (MstoKmh)
+    'kmh -> ms (KmhtoMs)
+
+    'mmHg ->Pa (mmHgtoPa)
+    'Pa ->mmHg (PatommHg)
+
+    'Hp -> W (HptoW)
+    'W -> Hp (WtoHp)
+
+    'J -> Cal (JtoCal)
+    'Cal -> J (CaltoJ)
+
+
+    Dim _firstValue, _answerValue As Double
+    Dim _symbol As String
+
+    Public Function conversion(ByVal _firstValue, ByVal _symbol) As Object
+
+        Try
+
+            Select Case _symbol
+
+                Case "IntoCm"
+                    _answerValue = _firstValue * 2.5 '//Inch to Cm
+                Case "CmtoIn"
+                    _answerValue = _firstValue * (1 / 2.5)  '//Cm to Inch
+
+                Case "MstoKmh"
+                    _answerValue = _firstValue * (1 / 0.28) '//Mps to Kmph
+                Case "KmhtoMs"
+                    _answerValue = _firstValue * 0.28 '//Kmph to Mps
+
+                Case "mmHgtoPa"
+                    _answerValue = _firstValue * (133.322368) '//mmHg to Pa
+                Case "PatommHg"
+                    _answerValue = _firstValue * (1 / 133.322368) '//Pa to mmHg
+
+                Case "HptoW"
+                    _answerValue = _firstValue * 745.699872 '//hp to W
+                Case "WtoHp"
+                    _answerValue = _firstValue * (1 / 745.699872) '//W to Hp
+
+                Case ""
+                    _answerValue = "There is no value to convert"
+
+            End Select
+
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+        Return _answerValue
+
+    End Function
+
+End Class
---
+++ Conversion/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("CSSCal 0.1 Unit Conversion")> 
+<Assembly: AssemblyDescription("CSSCal 0.1 ,SAITM ,BLD Dilanga")> 
+<Assembly: AssemblyCompany("by BLD Dilanga")> 
+<Assembly: AssemblyProduct("CSSCal 0.1 Unit Conversion")> 
+<Assembly: AssemblyCopyright("Copyright © by BLD Dilanga 2010")> 
+<Assembly: AssemblyTrademark("CSSCal 0.1 Unit Conversion")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("e9c4bdb0-8212-4cd3-beb2-e517726cd232")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
